05f44be2e655edabc5be0c4cca51c44e49eb3284
João <joaovictorramalho7@gmail.com>
2020-09-09 17:02:55 -0300

    ex006

---
+++ ex006.py
@@ -0,0 +1,6 @@
+import math
+
+number = int(input('Digite um valor: '))
+print(f'O dobro é:         {number*2}')
+print(f'O triplo é:        {number*3}')
+print(f'A raiz quadrada é: {pow(number, (1/2)):.2f}')
