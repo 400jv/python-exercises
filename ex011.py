0a92a19cef68a8f5005ec3e57a492d66f1c14d86
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-09-11 16:39:23 -0300

    ex011

---
+++ ex011.py
@@ -0,0 +1,5 @@
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+
+print(f'Sua parede tem a dimensão de {larg}x{alt} e sua área é de {larg*alt}m²')
+print(f'Para pintar essa parede, você precisa de {larg*alt/2}l de tinta')
